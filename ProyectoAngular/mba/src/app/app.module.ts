4f8b45be82e3da2f69fbfd72efbe6a75b9eb2890
Alejandro Diaz Torres <alejandrodiaz1@upb.edu>
2023-12-04 18:28:42 -0400

    Admin-Program-001 Create Components

--- ProyectoAngular/mba/src/app/app.module.ts
+++ ProyectoAngular/mba/src/app/app.module.ts
@@ -7,6 +7,10 @@ import { LoginComponent } from './pages/login/login.component';
 import { HomeAdmiComponent } from './pages/home-admi/home-admi.component';
 import { HomeStudentComponent } from './pages/home-student/home-student.component';
 import { HomeTeacherComponent } from './pages/home-teacher/home-teacher.component';
+import { AdminProgramasComponent } from './pages/admin-programas/admin-programas.component';
+import { CrearProgramaComponent } from './pages/crear-programa/crear-programa.component';
+import { EditarProgramaComponent } from './pages/editar-programa/editar-programa.component';
+import { AgregarMateriaComponent } from './pages/agregar-materia/agregar-materia.component';
 
 @NgModule({
   declarations: [
@@ -15,6 +19,10 @@ import { HomeTeacherComponent } from './pages/home-teacher/home-teacher.componen
     HomeAdmiComponent,
     HomeStudentComponent,
     HomeTeacherComponent,
+    AdminProgramasComponent,
+    CrearProgramaComponent,
+    EditarProgramaComponent,
+    AgregarMateriaComponent,
   ],
   imports: [
     BrowserModule,
---
+++ ProyectoAngular/mba/src/app/pages/admin-programas/admin-programas.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-admin-programas',
+  templateUrl: './admin-programas.component.html',
+  styleUrl: './admin-programas.component.css'
+})
+export class AdminProgramasComponent {
+
+}
