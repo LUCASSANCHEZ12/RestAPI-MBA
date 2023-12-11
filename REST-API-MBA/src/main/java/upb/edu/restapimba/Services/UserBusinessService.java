a7075c07761f466eb588aa2cc4ec4a3ae45a41a7
LUCASSANCHEZ12 <lucassanchez1@upb.edu>
2023-12-11 08:54:25 -0400

    REST API >> Added new class ChatBot.py for the MBA ChatGPT chatbot

--- REST-API-MBA/src/main/java/upb/edu/restapimba/Services/UserBusinessService.java
+++ REST-API-MBA/src/main/java/upb/edu/restapimba/Services/UserBusinessService.java
@@ -31,49 +31,41 @@ public class UserBusinessService implements UsersBusinessServiceInterface
 
     @Override
     public UserModel getByCode(long code) {
-        // TODO Auto-generated method stub
         return usersDao.getByCode(code);
     }
 
     @Override
     public boolean verifyCredentials(long code, String passwd) {
-        // TODO Auto-generated method stub
         return usersDao.verifyCredentials(code, passwd);
     }
 
     @Override
     public CargoModel getCargo(long user) {
-        // TODO Auto-generated method stub
         return usersDao.getCargo(user);
     }
 
     @Override
     public List<UserModel> getUsers() {
-        // TODO Auto-generated method stub
         return usersDao.getUsers();
     }
 
     @Override
     public List<UserModel> searchUsers(String searchTerm) {
-        // TODO Auto-generated method stub
         return usersDao.searchUsers(searchTerm);
     }
 
     @Override
     public UserModel createUser(UserModel userModel) {
-        // TODO Auto-generated method stub
         return usersDao.createUser(userModel);
     }
 
     @Override
     public UserModel updateUser(UserModel userModel) {
-        // TODO Auto-generated method stub
         return usersDao.updateUser(userModel);
     }
 
     @Override
     public UserModel deleteOneUser(long id) {
-        // TODO Auto-generated method stub
         return usersDao.deleteOneUser(id);
     }
 
